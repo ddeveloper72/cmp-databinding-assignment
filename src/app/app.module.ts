8ecce8afcf997bf483acd479df4311f0ef92a127
ddeveloper72 <duncan.niamh@gmail.com>
2019-06-01 11:27:22 +0100

    Created component for odd numbers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponentComponent } from './odd-component/odd-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameControlComponent
+    GameControlComponent,
+    OddComponentComponent
   ],
   imports: [
     BrowserModule
