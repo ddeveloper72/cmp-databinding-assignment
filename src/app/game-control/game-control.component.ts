ff8729bec87782cd60bedad382052cc1dcc68ecf
ddeveloper72 <duncan.niamh@gmail.com>
2019-06-01 15:17:14 +0100

    Added onStartTimer() method, an eventEmitter for the  number and an incrementation counter from zero.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() timesUps = new EventEmitter<number>();
+  duration; // property for timer
+  lastNumber = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartTimer() {
+    this.duration = setInterval(() => {
+      this.timesUps.emit(this.lastNumber);
+      this.lastNumber++;
+    }, 1000);
   }
 
 }
