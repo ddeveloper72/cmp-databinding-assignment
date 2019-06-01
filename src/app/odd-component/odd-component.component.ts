7298e6e7a1b6421fff1402ba2acda96db7849c2b
ddeveloper72 <duncan.niamh@gmail.com>
2019-06-01 16:29:50 +0100

    Added number and allowed number to received externally without an alias

--- src/app/odd-component/odd-component.component.ts
+++ src/app/odd-component/odd-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-odd-component',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./odd-component.component.css']
 })
 export class OddComponentComponent implements OnInit {
+  @Input() number: number;
 
   constructor() { }
 
