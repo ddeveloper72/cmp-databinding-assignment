79049d6e0bff04eda87899cbd3edc77bac0f5534
ddeveloper72 <duncan.niamh@gmail.com>
2019-06-01 15:24:45 +0100

    Log the last number, on timer started.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmp-databinding-assignment';
+  onStartTimerStarted(passedNumber: number) {
+    console.log(passedNumber);
+  }
 }
